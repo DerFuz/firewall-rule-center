a52d0b93844eea08697bd59b2e1f08af43e37950
Jakob Woelfl <11327876+DerFuz@users.noreply.github.com>
2023-08-26 22:46:55 +0200

    registered models to admin-view

--- backend/rules/admin.py
+++ backend/rules/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rule, RuleAction, RuleProtocol, RuleStatus
+
+admin.site.register(Rule)
+admin.site.register(RuleAction)
+admin.site.register(RuleProtocol)
+admin.site.register(RuleStatus)
