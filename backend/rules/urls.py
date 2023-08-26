4a8109a780ff23a8c8b47d7ec5c8b7cbf7148ee5
Jakob Woelfl <11327876+DerFuz@users.noreply.github.com>
2023-08-26 22:45:37 +0200

    add name to rule-delete path

--- backend/rules/urls.py
+++ backend/rules/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.rule_list_create_view, name='rule-list'),
     path('<int:pk>/update/', views.rule_update_view, name='rule-edit'),
-    path('<int:pk>/delete/', views.rule_delete_view),
+    path('<int:pk>/delete/', views.rule_delete_view, name='rule-delete'),
     path('<int:pk>/', views.rule_detail_view, name='rule-detail'),
 ]
