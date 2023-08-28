816124abf07dd98e090c5299788f00057bb65a56
Jakob Woelfl <11327876+DerFuz@users.noreply.github.com>
2023-08-28 22:36:13 +0200

    add logic validation for source and destination input

--- backend/rules/serializers.py
+++ backend/rules/serializers.py
@@ -17,18 +17,16 @@ class ProductInlineSerializer(serializers.Serializer):
 class RuleSerializer(serializers.ModelSerializer):
     #requester = UserPublicSerializer(source = "user", read_only = True) # good
     #related_products = ProductInlineSerializer(source = "user.product_set.all", read_only = True, many = True)
-    #my_user_data = serializers.SerializerMethodField(read_only = True) # bad
-    #my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     delete_url = serializers.SerializerMethodField(read_only=True)
     #url = serializers.HyperlinkedIdentityField(
     #    view_name='product-detail',
     #    lookup_field='pk'
     #    )
-    source_ip_orig = serializers.CharField(validators=[validators.validate_ip])
-    source_ip_nat = serializers.CharField(validators=[validators.validate_ip])
-    destination_ip_orig = serializers.CharField(validators=[validators.validate_ip])
-    destination_ip_nat = serializers.CharField(validators=[validators.validate_ip])
+    source_ip_orig = serializers.CharField(required=False, validators=[validators.validate_ip])
+    source_ip_nat = serializers.CharField(required=False, validators=[validators.validate_ip])
+    destination_ip_orig = serializers.CharField(required=False, validators=[validators.validate_ip])
+    destination_ip_nat = serializers.CharField(required=False, validators=[validators.validate_ip])
     class Meta:
         model = Rule
         fields = '__all__'
@@ -47,14 +45,23 @@ class RuleSerializer(serializers.ModelSerializer):
     #         "username": obj.user.username
     #     }
 
-    def get_edit_url(self, obj):
+
+    def validate(self, data):
+        if not data.get('source_ip_orig') and not data.get('source_ip_nat'):
+            raise serializers.ValidationError('at least one source input is required')
+        if not data.get('destination_ip_orig') and not data.get('destination_ip_nat'):
+            raise serializers.ValidationError('at least one destination input is required')
+        return data
+
+
+    def get_edit_url(self, obj) -> str:
         # return f"/api/rules/{obj.pk}/update/"
         request = self.context.get('request')
         if request is None:
             return None
         return reverse("rule-edit", kwargs={"pk": obj.pk}, request=request)
     
-    def get_delete_url(self, obj):
+    def get_delete_url(self, obj) -> str:
         # return f"/api/rules/{obj.pk}/delete/"
         request = self.context.get('request')
         if request is None:
