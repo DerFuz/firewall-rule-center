506537cd4e910416ebb5dc05d3e61f6fd930eb22
Jakob Woelfl <11327876+DerFuz@users.noreply.github.com>
2024-01-21 16:15:18 +0100

    changed user model reference in rulesetrequest model

--- backend/rulesetrequests/models.py
+++ backend/rulesetrequests/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.conf import settings
 from simple_history.models import HistoricalRecords
-
-User = settings.AUTH_USER_MODEL # auth.user
+from django.contrib.auth import get_user_model
 
 class RuleSetRequest(models.Model):
     # Status Choices
@@ -22,19 +21,19 @@ class RuleSetRequest(models.Model):
     status = models.CharField(max_length=3, choices=RULESETREQUEST_STATUS_CHOICES, default=REQUESTED)
 
     # approver of this rulesetrequest entry
-    approver = models.ForeignKey(User, related_name='rulesetrequest_approved_by', blank=True, null=True, on_delete=models.PROTECT)
+    approver = models.ForeignKey(get_user_model(), related_name='rulesetrequest_approved_by', blank=True, null=True, on_delete=models.PROTECT)
 
     # RuleSetRequest entry creation timestamp
     created_on = models.DateTimeField(auto_now_add=True)
     
     # Creator of this rulesetrequest entry
-    created_by = models.ForeignKey(User, related_name='rulesetrequest_created_by', on_delete=models.PROTECT)
+    created_by = models.ForeignKey(get_user_model(), related_name='rulesetrequest_created_by', on_delete=models.PROTECT)
 
     # RuleSetRequest entry update timestamp
     last_updated_on = models.DateTimeField(auto_now=True)
 
     # Last user that updated this RuleSetRequest entry
-    last_updated_by = models.ForeignKey(User, related_name='rulesetrequest_last_updated_by', on_delete=models.PROTECT)
+    last_updated_by = models.ForeignKey(get_user_model(), related_name='rulesetrequest_last_updated_by', on_delete=models.PROTECT)
 
     # historical records
     history = HistoricalRecords()
